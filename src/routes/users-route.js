51788678462edab4941f00331862af2c891e243f
Helmi-Qatqat <helmiqatqat@gmail.com>
2023-09-05 10:15:51 +0000

    authenticate using jsonwebtoken

--- src/routes/users-route.js
+++ src/routes/users-route.js
@@ -2,10 +2,11 @@
 
 const express = require('express')
 const UsersRouter = express.Router();
-const {SignupHandler, LoginHandler, UpdateHandler, TimeRecordsHandler} = require('../handlers/user-handlers');
+const {SignupHandler, LoginHandler, UpdateHandler, TimeRecordsHandler, UserDataHandler} = require('../handlers/user-handlers');
 
 UsersRouter.post('/api/signup', SignupHandler);
 UsersRouter.post('/api/login', LoginHandler);
+UsersRouter.get('/api/user-data', UserDataHandler);
 UsersRouter.put('/api/update', UpdateHandler);
 UsersRouter.post('/api/time-records', TimeRecordsHandler);
 
